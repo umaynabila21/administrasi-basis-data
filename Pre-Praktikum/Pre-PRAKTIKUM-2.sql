cc82eebcd9fa28a3a254805278fcf39269a1fc73
umaynabila21 <146051177+umaynabila21@users.noreply.github.com>
2025-04-27 16:43:51 +0700

    Add files via upload

---
+++ Pre-Praktikum/Pre-PRAKTIKUM-2.sql
@@ -0,0 +1,10 @@
+-- LIHAT DATABASES --
+SHOW DATABASES;
+-- MENGGUNAKAN DATABASES mysql
+USE mysql;
+-- MELIHAT ISI TABEL DALAM DB mysql
+SHOW TABLES;
+-- MELIHAT DESKRIPSI DARI TABEL user
+DESCRIBE user;
+-- ambil isi dari kolom Host, User, Password
+SELECT Host, User, Password FROM User;
